f479c889a50fa258073fed8c3c235ddaa90b2b47
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-01-23 21:13:22 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='bg-slate-800 min-h-screen flex justify-evenly items-center'>
+      <h1>Hello Tic Tac Toe</h1>
+    </div>
+  )
+}
+
+export default App
